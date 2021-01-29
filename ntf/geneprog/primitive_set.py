949ff5d098c951f3f68a7cbd1d94bb9cc81c6082
Yu-Hang "Maxin" Tang <Tang.Maxin@gmail.com>
2021-01-29 10:55:49 -0800

    Adding documentation.

--- ntf/geneprog/primitive_set.py
+++ ntf/geneprog/primitive_set.py
@@ -15,42 +15,28 @@ class PrimitiveSet:
     ----------
     ret_type: type
         Type of the overall factorization expression.
-    rank_types: list
-        The types of the vectors that make up one 'rank'.
-    k: int
-        Maximum number of ranks to be sought in the factorization.
     '''
 
     @staticmethod
     def new_type(name=None, bases=()):
+        '''A utility to simplify the creation of abstract types. These abstract
+        types can be used to define the input and output of strong-typed GP
+        primitives.
+
+        Parameters
+        ----------
+        name (optional): str
+            Name of the type. If None, a random identifier will be assigned.
+        bases (optional): tuple
+            A tuple of base classes that the new type will inherit from.
+
+        Returns
+        -------
+        t: type
+            A new type.
+        '''
         name = name or f'type{uuid.uuid4().hex}'
         return type(name, bases, {})
-
-    class PrimitiveBase(ABC):
-
-        @property
-        @abstractmethod
-        def action(self):
-            pass
-
-        def __init__(self, *operands, shape=None):
-            self.operands = tuple(operands)
-
-        def forward(self, grad=False):
-            return self.action(*[o.forward(grad=grad) for o in self.operands])
-
-    class TerminalBase(ABC):
-
-        @property
-        @abstractmethod
-        def action(self):
-            pass
-
-        def __init__(self, shape):
-            self._value = self.action(shape)
-
-        def forward(self, grad=False):
-            return self._value
 
     def __init__(self, ret_type):
         self.ret_type = ret_type
@@ -58,6 +44,20 @@ class PrimitiveSet:
         self.hyperdep = {}
 
     def from_string(self, string):
+        '''Convert an expression in the form of a character string into a
+        tree of primitives using the current primitive set.
+
+        Parameters
+        ----------
+        string: str
+            A character string such as 'add(x, y)'.
+
+        Returns
+        -------
+        expr: :py:class:`gp.PrimitiveTree`
+            A tree representation of the primitives. The tree can also be
+            iterated over like a list in depth-first order.
+        '''
         return gp.PrimitiveTree.from_string(string, self.pset)
 
     def gen_expr(self, max_depth: int, p=None):
@@ -74,15 +74,15 @@ class PrimitiveSet:
         Returns
         -------
         expr: list
-            A factorization in the form of a prefix expression.
+            An abstract factorization in the form of a prefix expression.
         '''
-        return self._gen_expr(
+        return self._gen_expr_depth_first(
             self.ret_type,
             p if p is not None else lambda _: 1.0,
             max_depth
         )
 
-    def _gen_expr(self, t=None, p=None, d=0):
+    def _gen_expr_depth_first(self, t=None, p=None, d=0):
         if d <= 0:  # try to terminate ASAP
             try:
                 choice = np.random.choice(self.pset.terminals[t], 1).item()
@@ -98,17 +98,38 @@ class PrimitiveSet:
         if isinstance(choice, gp.Terminal):
             return [choice]
         else:
-            return [choice, *flatten([self._gen_expr(a, p=p, d=d-1)
+            return [choice, *flatten([self._gen_expr_depth_first(a, p=p, d=d-1)
                                       for a in choice.args])]
 
     def instantiate(self, expr, **hyper_params):
-        return self._instantiate(expr, **hyper_params)[0]
+        '''Create a concrete matrix factorization using the given expression.
 
-    def _instantiate(self, expr, **hyper_params):
+        Parameters
+        ----------
+        expr: list or :py:class:`gp.PrimitiveTree`
+            An expression representing a symbolic factorization.
+        hyper_params: keyword arguments
+            Hyperparameters to be forwarded to the primitives.
+
+        Returns
+        -------
+        f: callable
+            An object that when evaluated returns a reconstructed matrix from
+            the given factorization.
+        '''
+        try:
+            return self._instantiate_LL1(expr, **hyper_params)[0]
+        except Exception as e:
+            raise RuntimeError(
+                f'When instantiating the expression {expr}, the following '
+                f'exception occurred:\n\n{e}'
+            )
+
+    def _instantiate_LL1(self, expr, **hyper_params):
         primitive, tail_expr = expr[0], expr[1:]
         children = []
         for _ in range(primitive.arity):
-            child, tail_expr = self._instantiate(tail_expr, **hyper_params)
+            child, tail_expr = self._instantiate_LL1(tail_expr, **hyper_params)
             children.append(child)
         primitive_impl = self.pset.context[primitive.name]
         return primitive_impl(*children, **hyper_params), tail_expr
@@ -116,19 +137,20 @@ class PrimitiveSet:
     @staticmethod
     def _get_hyperspecs(f, name):
         arg_spec = inspect.getfullargspec(f)
-
         assert arg_spec.varargs is None, f'Variable-length hyperparameter \
             *{arg_spec.varargs} is not allowed for primitive {name}.'
         assert arg_spec.varkw is None, f'Variable-length keyword \
             hyperparameter **{arg_spec.varkw} is not allowed for primitive \
             {name}.'
-
         hyperparams = arg_spec.args[1:] + arg_spec.kwonlyargs
+
         hyperdefaults = {}
+        # defaults for positional arguments
         if arg_spec.defaults is not None:
             for key, value in zip(arg_spec.defaults[-1::-1],
                                   arg_spec.args[-1::-1]):
                 hyperdefaults[key] = value
+        # defaults for keyword-only arguments
         if arg_spec.kwonlydefaults is not None:
             hyperdefaults.update(**arg_spec.kwonlydefaults)
 
