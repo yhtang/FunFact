0d4ba92b1b60bdc9ce1d52341ec35f825e1c0671
Yu-Hang "Maxin" Tang <Tang.Maxin@gmail.com>
2021-05-25 14:28:25 -0700

    refactor

--- symfac/experimental/__init__.py
+++ symfac/experimental/__init__.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from .rbf_expansion import RBFExpansion
-from .rbf_expansion2 import RBFExpansionV2
-from .rbf_expansion3 import RBFExpansionMiniBatch
+from .rbf_expansion_minibatch import RBFExpansionMiniBatch
 
+
+RBFExpansionV2 = RBFExpansion
 
 __all__ = ['RBFExpansion', 'RBFExpansionV2', 'RBFExpansionMiniBatch']
--- symfac/experimental/rbf_expansion.py
+++ symfac/experimental/rbf_expansion.py
@@ -7,238 +7,204 @@ import warnings
 from .rbf_expansion_base import RBFExpansionBase
 
 
-# class RBFExpansion(RBFExpansionBase):
+class RBFExpansion(RBFExpansionBase):
 
-#     def __init__(
-#         self, k=1, rbf='gauss', batch_size=64, max_steps=10000, loss='mse_loss',
-#         algorithm='Adam', lr=0.1, device='auto', progressbar='default'
-#     ):
-#         self.k = k
+    def __init__(
+        self, k=1, rbf='gauss', batch_size=64, max_steps=10000, loss='mse_loss',
+        algorithm='Adam', lr=0.1, device='auto', progressbar='default'
+    ):
+        self.k = k
 
-#         if callable(rbf):
-#             self.rbf = rbf
-#         elif rbf == 'gauss':
-#             self.rbf = lambda d: torch.exp(-torch.square(d))
-#         else:
-#             raise f'Unrecoginized RBF: {rbf}.'
+        if callable(rbf):
+            self.rbf = rbf
+        elif rbf == 'gauss':
+            self.rbf = lambda d: torch.exp(-torch.square(d))
+        else:
+            raise f'Unrecoginized RBF: {rbf}.'
 
-#         self.batch_size = batch_size
-#         self.max_steps = max_steps
+        self.batch_size = batch_size
+        self.max_steps = max_steps
 
-#         if isinstance(loss, str):
-#             try:
-#                 self.loss = getattr(torch.nn.functional, loss)
-#             except AttributeError:
-#                 raise AttributeError(
-#                     f'The loss function \'{loss}\' does not exist in'
-#                     'torch.nn.functional.'
-#                 )
-#         else:
-#             self.loss = loss
-#         try:
-#             self.loss(torch.zeros(1), torch.zeros(1))
-#         except Exception as e:
-#             raise AssertionError(
-#                 f'The given loss function does not accept two arguments:\n{e}'
-#             )
+        if isinstance(loss, str):
+            try:
+                self.loss = getattr(torch.nn.functional, loss)
+            except AttributeError:
+                raise AttributeError(
+                    f'The loss function \'{loss}\' does not exist in'
+                    'torch.nn.functional.'
+                )
+        else:
+            self.loss = loss
+        try:
+            self.loss(torch.zeros(1), torch.zeros(1))
+        except Exception as e:
+            raise AssertionError(
+                f'The given loss function does not accept two arguments:\n{e}'
+            )
 
-#         if isinstance(algorithm, str):
-#             try:
-#                 self.algorithm = getattr(torch.optim, algorithm)
-#             except AttributeError:
-#                 raise AttributeError(
-#                     f'Cannot find \'{algorithm}\' in torch.optim.'
-#                 )
-#         else:
-#             self.algorithm = algorithm
+        if isinstance(algorithm, str):
+            try:
+                self.algorithm = getattr(torch.optim, algorithm)
+            except AttributeError:
+                raise AttributeError(
+                    f'Cannot find \'{algorithm}\' in torch.optim.'
+                )
+        else:
+            self.algorithm = algorithm
 
-#         self.lr = lr
+        self.lr = lr
 
-#         self.device = self._get_device(device)
+        self.device = self._get_device(device)
 
-#         if progressbar == 'default':
-#             self.progressbar = lambda n: tqdm.trange(
-#                 n, miniters=None, mininterval=0.25, leave=True
-#             )
-#         else:
-#             self.progressbar = progressbar
+        if progressbar == 'default':
+            self.progressbar = lambda n: tqdm.trange(
+                n, miniters=None, mininterval=0.25, leave=True
+            )
+        else:
+            self.progressbar = progressbar
 
-#     def fit(self, target, u0=None, v0=None, a0=None, b0=None, seed=None):
+    def fit(self, target, u0=None, v0=None, a0=None, b0=None, seed=None):
 
-#         with torch.random.fork_rng(devices=[self.device]):
-#             if seed:
-#                 torch.random.manual_seed(seed)
+        with torch.random.fork_rng(devices=[self.device]):
+            if seed:
+                torch.random.manual_seed(seed)
 
-#             target = torch.as_tensor(target, device=self.device).unsqueeze(0)
-#             _, n, m = target.shape
+            target = torch.as_tensor(target, device=self.device).unsqueeze(0)
+            _, n, m = target.shape
 
-#             u0 = self.as_tensor(u0) if u0 is not None else \
-#                 self.randn(self.batch_size, n, self.k)
-#             v0 = self.as_tensor(v0) if v0 is not None else \
-#                 self.randn(self.batch_size, m, self.k)
-#             a0 = self.as_tensor(a0) if a0 is not None else \
-#                 self.randn(self.batch_size, self.k)
-#             b0 = self.as_tensor(b0) if b0 is not None else \
-#                 self.randn(self.batch_size)
+            u0 = self.as_tensor(u0) if u0 is not None else \
+                self.randn(self.batch_size, n, self.k)
+            v0 = self.as_tensor(v0) if v0 is not None else \
+                self.randn(self.batch_size, m, self.k)
+            a0 = self.as_tensor(a0) if a0 is not None else \
+                self.randn(self.batch_size, self.k)
+            b0 = self.as_tensor(b0) if b0 is not None else \
+                self.randn(self.batch_size)
 
-#             def f(u, v, a, b):
-#                 return torch.sum(
-#                     self.rbf(u[..., :, None, :] - v[..., None, :, :]) *
-#                     a[..., None, None, :],
-#                     dim=-1
-#                 ) + b[..., None, None]
+            def f(u, v, a, b):
+                return torch.sum(
+                    self.rbf(u[..., :, None, :] - v[..., None, :, :]) *
+                    a[..., None, None, :],
+                    dim=-1
+                ) + b[..., None, None]
 
-#             self.optimum = self._grad_opt(target, f, u0, v0, a0, b0)
+            self.report = self._grad_opt(
+                target,
+                self.Model(f, (u0, v0, a0, b0), default_grad_on=True)
+            )
 
-#             self.result = (f, self.optimum.x, ['u', 'v', 'a', 'b'])
+            self._optimum = self.Model(
+                f, self.report.x_best, x_names=['u', 'v', 'a', 'b'],
+                default_device='cpu'
+            )
 
-#             return self
+            return self
 
-#     def fith(self, target, u0=None, a0=None, b0=None, seed=None):
+    def fith(self, target, u0=None, a0=None, b0=None, seed=None):
 
-#         with torch.random.fork_rng(devices=[self.device]):
-#             if seed:
-#                 torch.random.manual_seed(seed)
+        with torch.random.fork_rng(devices=[self.device]):
+            if seed:
+                torch.random.manual_seed(seed)
 
-#             target = torch.as_tensor(target, device=self.device).unsqueeze(0)
-#             _, n, m = target.shape
-#             assert n == m
+            target = torch.as_tensor(target, device=self.device).unsqueeze(0)
+            _, n, m = target.shape
+            assert n == m
 
-#             u0 = self.as_tensor(u0) if u0 is not None else \
-#                 self.randn(self.batch_size, n, self.k)
-#             a0 = self.as_tensor(a0) if a0 is not None else \
-#                 self.randn(self.batch_size, self.k)
-#             b0 = self.as_tensor(b0) if b0 is not None else \
-#                 self.randn(self.batch_size)
+            u0 = self.as_tensor(u0) if u0 is not None else \
+                self.randn(self.batch_size, n, self.k)
+            a0 = self.as_tensor(a0) if a0 is not None else \
+                self.randn(self.batch_size, self.k)
+            b0 = self.as_tensor(b0) if b0 is not None else \
+                self.randn(self.batch_size)
 
-#             def f(u, a, b):
-#                 return torch.sum(
-#                     self.rbf(u[..., :, None, :] - u[..., None, :, :]) *
-#                     a[..., None, None, :],
-#                     dim=-1
-#                 ) + b[..., None, None]
+            def f(u, a, b):
+                return torch.sum(
+                    self.rbf(u[..., :, None, :] - u[..., None, :, :]) *
+                    a[..., None, None, :],
+                    dim=-1
+                ) + b[..., None, None]
 
-#             self.optimum = self._grad_opt(target, f, u0, a0, b0)
+            self.report = self._grad_opt(
+                target,
+                self.Model(f, (u0, a0, b0), default_grad_on=True)
+            )
 
-#             self.result = (f, self.optimum.x, ['u', 'a', 'b'])
+            self._optimum = self.Model(
+                f, self.report.x_best, x_names=['u', 'a', 'b'],
+                default_device='cpu'
+            )
 
-#             return self
+            return self
 
-#     def fit_custom(self, target, f, seed=None, **x0):
+    def fit_custom(self, target, f, seed=None, **x0):
 
-#         with torch.random.fork_rng(devices=[self.device]):
-#             if seed:
-#                 torch.random.manual_seed(seed)
+        with torch.random.fork_rng(devices=[self.device]):
+            if seed:
+                torch.random.manual_seed(seed)
 
-#             target = torch.as_tensor(target, device=self.device).unsqueeze(0)
-#             _, n, m = target.shape
+            target = torch.as_tensor(target, device=self.device).unsqueeze(0)
+            _, n, m = target.shape
 
-#             x = [self.as_tensor(w) for w in x0.values()]
+            x = [self.as_tensor(w) for w in x0.values()]
 
-#             self.optimum = self._grad_opt(target, f, *x)
+            self.report = self._grad_opt(
+                target,
+                self.Model(f, x, default_grad_on=True)
+            )
 
-#             self.result = (f, self.optimum.x, list(x0.keys()))
+            self._optimum = self.Model(
+                f, self.report.x_best, x_names=list(x0.keys()),
+                default_device='cpu'
+            )
 
-#             return self
+            return self
 
-#     @property
-#     def optimum(self):
-#         return self._optimum
+    def _grad_opt(self, target, model):
 
-#     @optimum.setter
-#     def optimum(self, opt_dict):
-#         self._optimum = as_namedtuple('optimum', **opt_dict)
+        try:
+            opt = self.algorithm(model.x, lr=self.lr)
+        except Exception:
+            raise AssertionError(
+                'Cannot instance optimizer of type {self.algorithm}:\n{e}'
+            )
 
-#     @property
-#     def result(self):
-#         return self._result
+        data_dim = list(range(1, len(target.shape)))
+        report = {}
+        report['x_best'] = [w.clone().detach() for w in model.x]
+        report['t_best'] = torch.zeros(self.batch_size, dtype=torch.int)
+        report['loss_history'] = []
+        report['loss_history_ticks'] = []
+        for step in self.progressbar(self.max_steps):
+            opt.zero_grad()
+            output = model()
+            with warnings.catch_warnings():
+                warnings.simplefilter('ignore')
+                loss_batch = self.loss(
+                    output, target, reduction='none'
+                ).mean(
+                    data_dim
+                )
 
-#     @result.setter
-#     def result(self, fxv):
-#         self._result = self.Batch(*fxv)
+            with torch.no_grad():
+                loss_cpu = loss_batch.detach().cpu()
+                report['loss_history'].append(loss_cpu.numpy())
+                if 'loss_best' in report:
+                    report['loss_best'] = torch.minimum(
+                        report['loss_best'], loss_cpu
+                    )
+                else:
+                    report['loss_best'] = loss_cpu
+                better = report['loss_best'] == loss_cpu
+                report['loss_history_ticks'].append(step)
+                report['t_best'][better] = step
+                for current, new in zip(report['x_best'], model.x):
+                    current[better, ...] = new[better, ...]
 
-#     def _grad_opt(self, target, f, *x):
+            loss_batch.sum().backward()
+            opt.step()
 
-#         try:
-#             opt = self.algorithm(x, lr=self.lr)
-#         except Exception:
-#             raise AssertionError(
-#                 'Cannot instance optimizer of type {self.algorithm}:\n{e}'
-#             )
+        report['loss_history'] = np.array(
+            report['loss_history'], dtype=np.float
+        )
 
-#         data_dim = list(range(1, len(target.shape)))
-#         optimum = {}
-#         optimum['x'] = [w.clone().detach() for w in x]
-#         optimum['t'] = torch.zeros(self.batch_size, dtype=torch.int)
-#         optimum['history'] = []
-#         for step in self.progressbar(self.max_steps):
-#             opt.zero_grad()
-#             output = f(*x)
-#             with warnings.catch_warnings():
-#                 warnings.simplefilter('ignore')
-#                 loss_batch = self.loss(
-#                     output, target, reduction='none'
-#                 ).mean(
-#                     data_dim
-#                 )
-
-#             with torch.no_grad():
-#                 loss_cpu = loss_batch.detach().cpu()
-#                 optimum['history'].append(loss_cpu.numpy())
-#                 if 'best_loss' in optimum:
-#                     optimum['best_loss'] = torch.minimum(
-#                         optimum['best_loss'], loss_cpu
-#                     )
-#                 else:
-#                     optimum['best_loss'] = loss_cpu
-#                 better = optimum['best_loss'] == loss_cpu
-#                 optimum['t'][better] = step
-#                 for current, new in zip(optimum['x'], x):
-#                     current[better, ...] = new[better, ...]
-
-#             loss_batch.sum().backward()
-#             opt.step()
-
-#         optimum['history'] = np.array(optimum['history'], dtype=np.float)
-#         return optimum
-
-#     class Batch:
-#         '''An approximation of a dense matrix as a sum of RBF over distance
-#         matrices.
-#         '''
-
-#         def __init__(self, f, x, vars):
-#             for w in x:
-#                 assert w.shape[0] == x[0].shape[0],\
-#                     "Inconsisent component size."
-#             self.f = f
-#             self.x = x
-#             self.vars = vars
-
-#         def __repr__(self):
-#             vars_str = ', '.join(self.vars)
-#             return f'<batch of {len(self)} RBF expansions [vars = {vars_str}]>'
-
-#         def __len__(self):
-#             return len(self.x[0])
-
-#         def __call__(self, runs=None, components=None, device='cpu'):
-#             with torch.no_grad():
-#                 x = self.x
-#                 if runs is not None:
-#                     x = [w[runs, ...] for w in x]
-#                 if components is not None:
-#                     components = torch.as_tensor(components)
-#                     if components.dim() == 0:
-#                         components = components.unsqueeze(0)
-#                     x = [w[..., components]
-#                          if len(w.shape) > 0 else torch.zeros_like(w)
-#                          for w in x]
-#                 return self.f(*x).to(device)
-
-#         @property
-#         def funrank(self):
-#             return len(self.x[-1])
-
-#         def __getattr__(self, a):
-#             return self.x[self.vars.index(a)]
+        return report
