a03c133009a7f63ee46b766758cd557a2e84a4e6
Yu-Hang "Maxin" Tang <Tang.Maxin@gmail.com>
2021-09-21 15:24:32 -0700

    bump version number

--- funfact/__init__.py
+++ funfact/__init__.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""NTF"""
+"""Fun-Fact"""
 
-__version__ = '0.5.6'
+__version__ = '0.5.7'
 __author__ = '''Yu-Hang "Maxin" Tang'''
 __maintainer__ = 'Yu-Hang "Maxin" Tang'
 __email__ = 'Tang.Maxin@gmail.com'
 __license__ = 'see LICENSE file'
-__copyright__ = '''Copyright (c) 2021'''
+__copyright__ = '''see COPYRIGHT file'''
