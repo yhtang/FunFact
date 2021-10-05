f9fdee883b17d46b82f68186ac4dd0717b44fe85
Yu-Hang "Maxin" Tang <Tang.Maxin@gmail.com>
2021-10-05 14:56:28 -0700

    comment out obsolete code

--- funfact/_interpreter.py
+++ funfact/_interpreter.py
@@ -47,47 +47,44 @@ class LatexReprInterpreter:
         return Evaluated(expr, value)
 
 
-class TraceInterpreter:
-    '''A trace interpreter is a meta-interpreter that invokes another
-    interpreter to generate an entire evaluated syntax tree.
+# class TraceInterpreter:
+#     '''A trace interpreter is a meta-interpreter that invokes another
+#     interpreter to generate an entire evaluated syntax tree.
 
-    '''
+#     '''
 
-    rules = {}
-    rules['idx'] = lambda tensor, *indices:\
-        fr'''{{{tensor}}}_{{{''.join(map(str, indices))}}}'''
-    rules['lit'] = lambda value: str(value)
-    rules['call'] = lambda input, f: fr'\operatorname{{{f}}}{{{input}}}'
-    rules['square'] = lambda input: fr'{input}^{2}'
-    rules['neg'] = lambda input: fr'-{input}'
-    rules['mul'] = lambda lhs, rhs: fr'{lhs} \times {rhs}'
-    rules['div'] = lambda lhs, rhs: f'{lhs} / {rhs}'
-    rules['add'] = lambda lhs, rhs: f'{lhs} + {rhs}'
-    rules['sub'] = lambda lhs, rhs: f'{lhs} - {rhs}'
+#     rules = {}
+#     rules['idx'] = lambda tensor, *indices:\
+#         fr'''{{{tensor}}}_{{{''.join(map(str, indices))}}}'''
+#     rules['lit'] = lambda value: str(value)
+#     rules['call'] = lambda input, f: fr'\operatorname{{{f}}}{{{input}}}'
+#     rules['square'] = lambda input: fr'{input}^{2}'
+#     rules['neg'] = lambda input: fr'-{input}'
+#     rules['mul'] = lambda lhs, rhs: fr'{lhs} \times {rhs}'
+#     rules['div'] = lambda lhs, rhs: f'{lhs} / {rhs}'
+#     rules['add'] = lambda lhs, rhs: f'{lhs} + {rhs}'
+#     rules['sub'] = lambda lhs, rhs: f'{lhs} - {rhs}'
 
-    def __call__(self, expr, interpret):
-        if hasattr(expr, '_repr_tex_'):
-            value = expr._repr_tex_()
-        else:
-            rule = self.rules[expr.p]
-            precedence = self.precedence[expr.p]
+#     def __call__(self, expr, interpret):
+#         if hasattr(expr, '_repr_tex_'):
+#             value = expr._repr_tex_()
+#         else:
+#             rule = self.rules[expr.p]
+#             precedence = self.precedence[expr.p]
 
-            parts = []
-            for arg in map(self, expr.args):
-                part = arg.value
+#             parts = []
+#             for arg in map(self, expr.args):
+#                 part = arg.value
 
-                try:
-                    if self.precedence[arg.expr.p] > precedence:
-                        part = fr'\left({part}\right)'
-                except AttributeError:
-                    pass
+#                 try:
+#                     if self.precedence[arg.expr.p] > precedence:
+#                         part = fr'\left({part}\right)'
+#                 except AttributeError:
+#                     pass
 
-                parts.append(part)
+#                 parts.append(part)
 
-            value = rule(*parts, **expr.params)
+#             value = rule(*parts, **expr.params)
 
-        return Evaluated(expr, value)
+#         return Evaluated(expr, value)
 
-# TODO: 
-# meta-interpreter, e.g. a tracer interpreter that converts any interpreter
-# into one that returns an interpreted tree.
