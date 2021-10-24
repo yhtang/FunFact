7676bee9ca92cd5c04c34b21246cdc2bd41324f2
Yu-Hang "Maxin" Tang <Tang.Maxin@gmail.com>
2021-10-23 22:21:51 -0700

    Simplification and factored repeated code.

--- funfact/lang/_interp_eval.py
+++ funfact/lang/_interp_eval.py
@@ -4,82 +4,67 @@ import numpy as np
 from ._interp_base import ROOFInterpreter
 from ._einop import _einop
 
+
+def _map_index(index_map, idx):
+    try:
+        return index_map[idx]
+    except KeyError:
+        index_map[idx] = chr(97 + len(index_map))
+        return index_map[idx]
+
+
 class EvaluationInterpreter(ROOFInterpreter):
-    '''The evaluation interpreter evaluates an initialized tensor expression. '''    
+    '''The evaluation interpreter evaluates an initialized tensor expression.
+    '''
+
     def scalar(self, value, payload):
         return value
 
     def tensor(self, value, payload):
         return payload[0]
-    
+
     def index(self, value, payload):
         return value.symbol
-    
+
     def index_notation(self, tensor, indices, payload):
         return (tensor, indices)
-    
+
     def call(self, f, x, payload):
-        return (getattr(np, f)(x[0]),payload[1])
+        return (getattr(np, f)(x[0]), payload[1])
 
     def pow(self, base, exponent, payload):
-        return (np.power(base[0], exponent),payload[1])
+        return (np.power(base[0], exponent), payload[1])
 
     def neg(self, x, payload):
-        return (-x[0],payload[1])
+        return (-x[0], payload[1])
+
+    @staticmethod
+    def _binary_operator(op, lhs, rhs, specs):
+        survive_indices, lhs_indices, rhs_indices = specs
+        index_map = {}
+        lhs_spec = ''.join([_map_index(index_map, idx) for idx in lhs_indices])
+        rhs_spec = ''.join([_map_index(index_map, idx) for idx in rhs_indices])
+        return (
+            _einop(f'{lhs_spec},{rhs_spec}', lhs, rhs, op),
+            survive_indices
+        )
 
     def div(self, lhs, rhs, payload):
-        index_map = {}
-        def get_mapped_index(idx):
-            try:
-                return index_map[idx]
-            except KeyError:
-                index_map[idx] = chr(97 + len(index_map))
-                return index_map[idx]
-        lhs_mapped = list(map(get_mapped_index, lhs[1]))
-        rhs_mapped = list(map(get_mapped_index, rhs[1]))
-        lhs_idx =''.join(lhs_mapped)
-        rhs_idx =''.join(rhs_mapped)
-        return (_einop(f'{lhs_idx},{rhs_idx}', lhs[0], rhs[0],np.divide), payload[1])
+        return self._binary_operator(
+            np.divide, lhs[0], rhs[0], (payload[1], lhs[1], rhs[1])
+        )
 
     def mul(self, lhs, rhs, payload):
-        index_map = {}
-        def get_mapped_index(idx):
-            try:
-                return index_map[idx]
-            except KeyError:
-                index_map[idx] = chr(97 + len(index_map))
-                return index_map[idx]
-        lhs_mapped = list(map(get_mapped_index, lhs[1]))
-        rhs_mapped = list(map(get_mapped_index, rhs[1]))
-        lhs_idx =''.join(lhs_mapped)
-        rhs_idx =''.join(rhs_mapped)
-        #return (jnp.einsum(f'{lhs_idx},{rhs_idx}', lhs[0], rhs[0]), payload[1])
-        return (_einop(f'{lhs_idx},{rhs_idx}', lhs[0], rhs[0],np.multiply), payload[1])
+        return self._binary_operator(
+            np.multiply, lhs[0], rhs[0], (payload[1], lhs[1], rhs[1])
+        )
 
     def add(self, lhs, rhs, payload):
-        index_map = {}
-        def get_mapped_index(idx):
-            try:
-                return index_map[idx]
-            except KeyError:
-                index_map[idx] = chr(97 + len(index_map))
-                return index_map[idx]
-        lhs_mapped = list(map(get_mapped_index, lhs[1]))
-        rhs_mapped = list(map(get_mapped_index, rhs[1]))
-        lhs_idx =''.join(lhs_mapped)
-        rhs_idx =''.join(rhs_mapped)
-        return (_einop(f'{lhs_idx},{rhs_idx}', lhs[0], rhs[0],np.add), payload[1])
+        return self._binary_operator(
+            np.add, lhs[0], rhs[0], (payload[1], lhs[1], rhs[1])
+        )
 
     def sub(self, lhs, rhs, payload):
-        index_map = {}
-        def get_mapped_index(idx):
-            try:
-                return index_map[idx]
-            except KeyError:
-                index_map[idx] = chr(97 + len(index_map))
-                return index_map[idx]
-        lhs_mapped = list(map(get_mapped_index, lhs[1]))
-        rhs_mapped = list(map(get_mapped_index, rhs[1]))
-        lhs_idx =''.join(lhs_mapped)
-        rhs_idx =''.join(rhs_mapped)
-        return (_einop(f'{lhs_idx},{rhs_idx}', lhs[0], rhs[0],np.subtract), payload[1])
+        return self._binary_operator(
+            np.subtract, lhs[0], rhs[0], (payload[1], lhs[1], rhs[1])
+        )
