b8fcdd23acb4ffc461487601a3e251a5361502e6
campsd <daancamps@gmail.com>
2021-11-17 16:21:51 -0800

    update test

--- funfact/model/test_factorization.py
+++ funfact/model/test_factorization.py
@@ -14,22 +14,22 @@ def test_elementwise():
     B = tensor('B', 2, 2)
     i, j, k = indices('i, j, k')
     tsrex = A[i, j] * B[j, k]
-    f = Factorization(tsrex)
+    fac = Factorization(tsrex)
     # one element
     idx = (1, 0)
-    full = f()[idx]
+    full = fac()[idx]
     elementwise = np.squeeze(f[idx])
     for f, e in zip([full], [elementwise]):
         assert pytest.approx(e, tol) == f
     # one row
     idx = (1, slice(None))
-    full = f()[idx]
+    full = fac()[idx]
     elementwise = np.squeeze(f[idx])
     for f, e in zip([full], [elementwise]):
         assert pytest.approx(e, tol) == f
     # one column
     idx = (slice(None), 0)
-    full = f()[idx]
+    full = fac()[idx]
     elementwise = np.squeeze(f[idx])
     for f, e in zip([full], [elementwise]):
         assert pytest.approx(e, tol) == f
@@ -38,17 +38,16 @@ def test_elementwise():
     A = tensor('A', 10)
     B = tensor('B', 5)
     tsrex = A[i] * B[j]
-    f = Factorization(tsrex)
-    f = Factorization(tsrex)
+    fac = Factorization(tsrex)
     # one element
     idx = (1, 0)
-    full = f()[idx]
+    full = fac()[idx]
     elementwise = np.squeeze(f[idx])
     for f, e in zip([full], [elementwise]):
         assert pytest.approx(e, tol) == f
     # slices
     idx = (slice(1, 6), slice(2, 4))
-    full = f()[idx]
+    full = fac()[idx]
     elementwise = np.squeeze(f[idx])
     for f, e in zip([full], [elementwise]):
         assert pytest.approx(e, tol) == f
