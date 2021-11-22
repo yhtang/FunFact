3df37d8a0723ad82d9a5b5e1f858ae4bdf73e9e5
campsd <daancamps@gmail.com>
2021-11-22 15:45:15 -0800

    shape interpreter

--- funfact/lang/interpreter/_shape.py
+++ funfact/lang/interpreter/_shape.py
@@ -31,22 +31,49 @@ class ShapeAnalyzer(TranscribeInterpreter):
 
     @as_payload
     def index_notation(self, tensor: P.tensor, indices: P.indices,  **kwargs):
-        return None
+        return tensor.shape
 
     @as_payload
     def call(self, f: str, x: Tensorial, **kwargs):
-        return None
+        return x.shape
 
     @as_payload
     def pow(self, base: Numeric, exponent: Numeric, **kwargs):
-        return None
+        if base.shape:
+            return base.shape
+        else:
+            return exponent.shape
 
     @as_payload
     def neg(self, x: Numeric, **kwargs):
-        return None
+        return x.shape
 
     @as_payload
     def ein(self, lhs: Numeric, rhs: Numeric, precedence: int, reduction: str,
             pairwise: str, outidx: Optional[P.indices], live_indices,
             **kwargs):
-        return None
+        dict_lhs = dict(zip(lhs.live_indices, lhs.shape))
+        dict_rhs = dict(zip(rhs.live_indices, rhs.shape))
+        shape = []
+        for i in live_indices:
+            if i in lhs.live_indices and i in rhs.live_indices:
+                if dict_lhs[i] != dict_rhs[i]:
+                    raise SyntaxError(
+                        f'Dimension of contracting index on left-hand side '
+                        f'({dict_lhs[i]}) does not match dimension of '
+                        f'right-hand side ({dict_lhs[i]}).'
+                    )
+                shape.append(dict_lhs[i])
+            elif i in lhs.live_indices:
+                shape.append(dict_lhs[i])
+            else:
+                shape.append(dict_rhs[i])
+        return tuple(shape)
+
+    @as_payload
+    def tran(self, src: Numeric, live_indices, **kwargs):
+        dict_src = dict(zip(src.live_indices, src.shape))
+        shape = []
+        for i in live_indices:
+            shape.append(dict_src[i])
+        return tuple(shape)
