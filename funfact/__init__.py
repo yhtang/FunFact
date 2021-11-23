f8e7727c07d30e605a1f34a57a205212b7a6330e
Yu-Hang "Maxin" Tang <Tang.Maxin@gmail.com>
2021-11-23 12:36:18 -0800

    bump version number and update changelog

--- funfact/__init__.py
+++ funfact/__init__.py
@@ -8,7 +8,7 @@ from .lang._math import *  # noqa: F401, F403
 __all__ = ['index', 'indices', 'tensor', 'template', '_0', '_1', 'delta']
 
 
-__version__ = '0.7a1'
+__version__ = '0.7a2'
 __author__ = '''Yu-Hang "Maxin" Tang, Daan Camps, Elizaveta Rebrova'''
 __maintainer__ = 'Yu-Hang "Maxin" Tang'
 __email__ = 'Tang.Maxin@gmail.com'
