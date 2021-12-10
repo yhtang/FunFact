f2b1a5feed81c7745cfe3d3f7bf8729d2bdcb6c7
Yu-Hang "Maxin" Tang <Tang.Maxin@gmail.com>
2021-12-09 22:58:42 -0800

    Update dependencies and RTD config.

--- setup.py
+++ setup.py
@@ -41,9 +41,9 @@ setup(
     ],
     extras_require={
         'docs': [
-            'sphinx',
-            'sphinx-rtd-theme',
-            'm2r2',
+            'mkdocs==1.2.3',
+            'mkdocs-material==8.0.5',
+            'mkdocstrings==0.16.2',
         ],
         'devel': [
             'expectexception>=0.1.1',
