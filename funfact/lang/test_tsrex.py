12584152af1cac671f5e19f6ee78f813fa3062c1
Yu-Hang "Maxin" Tang <Tang.Maxin@gmail.com>
2021-12-17 21:42:34 -0800

    100% coverage for `funfact.lang._tsrex`

--- funfact/lang/test_tsrex.py
+++ funfact/lang/test_tsrex.py
@@ -1,42 +1,96 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import pytest
+from copy import deepcopy
+from unittest.mock import MagicMock
 import itertools as it
 import numpy as np
-from ._tsrex import index, indices, tensor
+from ._tsrex import (
+    _BaseEx,
+    ArithmeticMixin,
+    TranspositionMixin,
+    TsrEx,
+    EinopEx,
+    IndexEx,
+    TensorEx,
+    index,
+    indices,
+    tensor
+)
 
 
-def test_abstract_tensor():
-    u = tensor(np.ones(3))
-    v = tensor('v', np.eye(4))
-    w = tensor(4, 2, 5)
-    x = tensor('x', 9, 2)
-    for t in [u, v, w, x]:
-        assert t.root.name == 'tensor'
-        assert hasattr(t.root.abstract, 'symbol')
-    # TODO: rewrite once [#31](https://github.com/yhtang/FunFact/issues/31)
-    # is addressed.
-    assert u.root.abstract.ndim == 1
-    assert v.root.abstract.ndim == 2
-    assert w.root.abstract.ndim == 3
-    assert x.root.abstract.ndim == 2
-    assert u.root.abstract.shape == (3,)
-    assert v.root.abstract.shape == (4, 4)
-    assert w.root.abstract.shape == (4, 2, 5)
-    assert x.root.abstract.shape == (9, 2)
+def test_baseex():
+
+    expr = tensor('testtensor', 2, 3, 4)
+
+    assert isinstance(expr, _BaseEx)
+    assert hasattr(expr, 'asciitree')
+    assert hasattr(expr, '_static_analyzed')
+    assert hasattr(expr, 'shape')
+    assert hasattr(expr, 'live_indices')
+    assert hasattr(expr, 'ndim')
+    assert hasattr(expr, 'einspec')
+    assert 'testtensor' in repr(expr.asciitree)
+    assert callable(expr.asciitree)
+    assert isinstance(expr.asciitree(stdout=False), str)
+    assert expr.asciitree(stdout=True) is None
+    assert expr.asciitree('name') is None
+    assert expr._repr_html_().startswith('$$')
+    assert expr._repr_html_().endswith('$$')
 
 
-def test_transposition():
+def test_arithmetic_mixin():
 
-    A = tensor('A', 2, 3, 4, 5)
-    i, j, k, r = indices('i, j, k, l')
-    for perm in it.permutations([i, j, k, r]):
-        AT = A[i, j, k, r] >> [*perm]
-        assert AT.root.name == 'tran'
-        # TODO: More tests once
-        # [#32](https://github.com/yhtang/FunFact/issues/32) is taken care of.
+    lhs = ArithmeticMixin()
+    rhs = ArithmeticMixin()
+    lhs.root = MagicMock(name='lhs')
+    rhs.root = MagicMock(name='rhs')
+
+    for ex in [
+        lhs + rhs, lhs + 1, 1 + rhs,
+        lhs - rhs, lhs - 1, 1 - rhs,
+        lhs * rhs, lhs * 1, 1 * rhs,
+        lhs / rhs, lhs / 1, 1 / rhs,
+    ]:
+        assert isinstance(ex, _BaseEx)
+        assert isinstance(ex, TsrEx)
+        assert isinstance(ex, EinopEx)
+        assert isinstance(ex, ArithmeticMixin)
+
+    for ex in [
+        lhs + rhs, lhs - rhs, lhs * rhs, lhs / rhs,
+    ]:
+        assert repr(lhs.root) in repr(ex.root)
+        assert repr(rhs.root) in repr(ex.root)
+
+    for ex in [
+        -lhs,
+        -(lhs + rhs),
+        -(lhs + 1),
+        -(1 + rhs),
+        -(1 * rhs)
+    ]:
+        assert isinstance(ex, _BaseEx)
+        assert isinstance(ex, TsrEx)
+        assert isinstance(ex, ArithmeticMixin)
+        assert ex.root.name == 'neg'
+
+    for ex in [
+        lhs**rhs,
+        (lhs + 2)**rhs,
+        lhs**1,
+        1**rhs,
+        lhs**(1 - rhs)
+    ]:
+        assert isinstance(ex, _BaseEx)
+        assert isinstance(ex, TsrEx)
+        assert isinstance(ex, ArithmeticMixin)
+        assert ex.root.name == 'pow'
 
 
+# YT: this test below should go into the shape analyzer
+# since we are only testing for the API here, not behavior
+'''
 def test_shape():
     A = tensor('A', 2, 3)
     B = tensor('B', 3, 4)
@@ -51,3 +105,191 @@ def test_shape():
     expected_shape = (6, 12)
     for t, e in zip(tsrex.shape, expected_shape):
         assert t == e
+'''
+
+
+def test_index_renaming_mixin():
+
+    a = tensor('a', 2, 3)
+    b = tensor('b', 3, 4)
+    i, j, k, p, q = indices('i, j, k, p, q')
+    e1 = a[i, j] * b[j, k]
+    e2 = e1[p, q]
+    assert isinstance(e2, TsrEx)
+    assert e2.root.name == e1.root.name
+    assert e1.live_indices[0] == i.root.item
+    assert e1.live_indices[1] == k.root.item
+    assert e2.live_indices[0] == p.root.item
+    assert e2.live_indices[1] == q.root.item
+
+    e3 = e1[j, q]
+    assert e3.root.name == e1.root.name
+    assert e3.live_indices[0] == j.root.item
+    assert e3.live_indices[1] == q.root.item
+
+    e4 = e1[j, i]
+    assert e4.root.name == e1.root.name
+    assert e4.live_indices[0] == j.root.item
+    assert e4.live_indices[1] == i.root.item
+
+    with pytest.raises(SyntaxError):
+        e1[i]
+    with pytest.raises(SyntaxError):
+        e1[i, j, k]
+    with pytest.raises(SyntaxError):
+        e1[i, b]
+    with pytest.raises(SyntaxError):
+        e1[b, b]
+
+
+def test_transposition_mixin():
+    a = TranspositionMixin()
+    a.root = MagicMock(name='mock_tensor')
+    for n in range(0, 9):
+        indices = [MagicMock(root='mock_index') for _ in range(n)]
+        b = a >> indices
+        assert isinstance(b, TsrEx)
+        assert b.root.name == 'tran'
+        assert repr(a.root) in repr(b.root)
+        for i in indices:
+            assert repr(i.root) in repr(b.root)
+
+
+def test_indexex():
+    i = index()
+    for j in [~i, *i]:
+        assert isinstance(j, IndexEx)
+
+    j = ~i
+    assert j.root.bound is True
+    assert j.root.kron is False
+
+    k, = [*i]
+    assert k.root.bound is False
+    assert k.root.kron is True
+
+    m, = [*(~i)]
+    assert m.root.bound is False
+    assert m.root.kron is True
+
+    n, = [*i]
+    n = ~n
+    assert n.root.bound is True
+    assert n.root.kron is False
+
+
+def test_tensorex():
+    t = tensor('u', 2, 3, 4)
+    i, j, k = indices(3)
+    ex = t[i, j, k]
+    assert isinstance(ex, TsrEx)
+    assert repr(t.root) in repr(ex.root)
+    assert repr(i.root) in repr(ex.root)
+    assert repr(j.root) in repr(ex.root)
+    assert repr(k.root) in repr(ex.root)
+
+
+def test_einop_ex():
+    e1 = EinopEx(MagicMock(root=MagicMock(outidx=[])))
+    e1_copy = deepcopy(e1)
+    i, j = [MagicMock(root='i'), MagicMock(root='j')]
+    e2 = e1 >> [i, j]
+    assert isinstance(e2, EinopEx)
+    assert e1.root == e1_copy.root
+    assert e2.root != e1.root
+    assert e2.root.outidx.items[0] == 'i'
+    assert e2.root.outidx.items[1] == 'j'
+
+
+def test_index():
+    i = index()
+    assert isinstance(i, IndexEx)
+    assert i.root.bound is False
+    assert i.root.kron is False
+
+    j = index('j')
+    assert isinstance(j, IndexEx)
+    assert str(j.root.item.symbol) == 'j'
+    assert j.root.bound is False
+    assert j.root.kron is False
+
+
+def test_indices():
+    i, = indices('i')
+    assert isinstance(i, IndexEx)
+
+    for n in range(10):
+        J = indices(n)
+        assert len(J) == n
+        for j in J:
+            assert isinstance(j, IndexEx)
+
+    p, q = indices('p, q')
+    assert isinstance(p, IndexEx)
+    assert isinstance(q, IndexEx)
+
+    r, s = indices('r s')
+    assert isinstance(r, IndexEx)
+    assert isinstance(s, IndexEx)
+
+    K = indices('a, b, c, d, e, f, g')
+    assert len(K) == 7
+
+    with pytest.raises(RuntimeError):
+        indices(-1)
+    with pytest.raises(RuntimeError):
+        indices(1.5)
+    with pytest.raises(RuntimeError):
+        indices('p+q')
+    with pytest.raises(RuntimeError):
+        indices(None)
+
+
+def test_tensor():
+
+    for n in range(1, 10):
+        u = tensor(np.ones(n))
+        assert isinstance(u, TensorEx)
+        assert u.ndim == 1
+        assert u.shape == (n,)
+
+        v = tensor(np.eye(n))
+        assert isinstance(v, TensorEx)
+        assert v.ndim == 2
+        assert v.shape == (n, n)
+
+
+@pytest.mark.parametrize(
+    'spec', [
+        ('x', np.random.randn(4, 3, 2)),
+        (np.random.randn(4, 3, 2),),
+        ('x', 4, 3, 2),
+        (4, 3, 2)
+    ]
+)
+def test_tensor_creation(spec):
+
+    t = tensor(*spec)
+    assert isinstance(t, TensorEx)
+    assert t.shape == (4, 3, 2)
+    assert t.ndim == 3
+
+
+def test_tensor_0d():
+    t = tensor('x')
+    assert isinstance(t, TensorEx)
+    assert t.shape == tuple()
+    assert t.ndim == 0
+
+
+@pytest.mark.parametrize(
+    'spec', [
+        (1.5,),
+        (2, 'x', 3, 4),
+        (2, 3, 'y')
+    ]
+)
+def test_tensor_creation_failure(spec):
+
+    with pytest.raises(RuntimeError):
+        tensor(*spec)
