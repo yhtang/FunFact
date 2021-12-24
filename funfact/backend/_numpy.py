9dd13c69f6d10d2f081fa7b6d5496f33111f6774
campsd <daancamps@gmail.com>
2021-12-24 09:20:53 -0800

    Numpy autograd error

--- funfact/backend/_numpy.py
+++ funfact/backend/_numpy.py
@@ -17,9 +17,27 @@ class NumPyBackend(metaclass=BackendMeta):
         return np.asarray(array, **kwargs)
 
     @classmethod
+    def to_numpy(cls, tensor):
+        return np.asarray(tensor)
+
+    @classmethod
     def seed(cls, key):
         cls._rng = np.random.default_rng(seed=key)
 
     @classmethod
     def normal(cls, mean, std, *shape, optimizable=False, dtype=np.float32):
         return cls._rng.normal(mean, std, shape)
+
+    @staticmethod
+    def loss_and_grad(loss_fn, example_model, example_target):
+        raise TypeError('NumPy backend does not support autograd and backward '
+                        'mode; use JAX or PyTorch backend instead.')
+
+    def autograd_decorator(ob):
+        return ob
+
+    class AutoGradMixin():
+        pass
+
+    def no_grad():
+        pass
