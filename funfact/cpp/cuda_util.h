414bf38b0333a302e5d77cd2a249288813eb94c8
Yu-Hang "Maxin" Tang <Tang.Maxin@gmail.com>
2021-12-28 10:51:49 -0800

    Removed the currently unused CUDA/CPP modules. Tagged the version before removal as `pre-removal-cuda-cpp` for future reference should we decide to bring back the JIT mechanism

--- funfact/cpp/cuda_util.h
+++
@@ -1,7 +0,0 @@
-constexpr static int warp_size = 32;
-
-template<class T> __device__ T warp_sum(T value) {
-    #pragma unroll
-    for(int p = (warp_size >> 1); p >= 1 ; p >>= 1) value += __shfl_xor_sync(0xFFFFFFFF, value, p);
-    return value;
-}
