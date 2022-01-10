28bccb7100a86a5ae7d9b3063e97cddc459953e8
Yu-Hang "Maxin" Tang <Tang.Maxin@gmail.com>
2022-01-09 20:44:31 -0800

    More tests.

--- funfact/lang/interpreter/test_einop.py
+++ funfact/lang/interpreter/test_einop.py
@@ -6,12 +6,31 @@ from ._einop import _einop
 
 
 @pytest.mark.parametrize('case', [
-    ((3, 2), (), 'ab,->ab'),  # right elementwise multiplication
-    ((), (3, 2), ',ab->ab'),  # left elementwise multiplication
-    ((), (), ',->'),  # scalar multiplication
-    ((3, 2), (3, 2), 'ab,ab->ab'),  # matrix elementwise multiplication
-    ((10,), (10,), 'i,i'),  # vector dot product
-    ((10, 3), (3, 10), 'ij,jk'),  # matrix inner product
+    # scalar multiplication
+    ((), (), ',->'),
+    # right elementwise multiplication
+    ((3,), (), 'a,->a'),
+    ((3,), (), 'a,->a'),
+    ((3,), (), 'a,'),
+    ((3, 2), (), 'ab,->ab'),
+    ((3, 2), (), 'ab,'),
+    # left elementwise multiplication
+    ((), (3,), ',a->a'),
+    ((), (3,), ',a->a'),
+    ((), (3,), ',a'),
+    ((), (3, 2), ',ab->ab'),
+    # vector dot product
+    ((10,), (10,), 'i,i'),
+    ((10,), (10,), 'i,i->i'),
+    # matrix elementwise multiplication
+    ((3, 2), (3, 2), 'ab,ab->ab'),
+    # inner product and contractions
+    ((10, 3), (3, 10), 'ij,jk'),
+    ((10, 3), (3, 10), 'ij,jk->ijk'),
+    ((2, 3, 4), (3, 4, 5), 'ijk,jkl'),
+    ((2, 3, 4), (3, 4), 'ijk,jk'),
+    ((2, 3, 4), (5, 4, 3), 'ijk,lkj'),
+    ((4, 3, 2), (5, 4, 3), 'ijk,lij'),
 ])
 def test_einsum(case):
     tol = 20 * ab.finfo(ab.float32).eps
