bff849304fd5ae972e45493a433d2742884f727d
Yu-Hang "Maxin" Tang <Tang.Maxin@gmail.com>
2022-01-09 21:56:54 -0800

    More tests

--- funfact/lang/interpreter/test_einop.py
+++ funfact/lang/interpreter/test_einop.py
@@ -33,12 +33,81 @@ from ._einop import _einop
     ((4, 3, 2), (5, 4, 3), 'ijk,lij'),
 ])
 def test_einsum(case):
-    tol = 20 * ab.finfo(ab.float32).eps
+    tol = 100 * ab.finfo(ab.float32).eps
 
     lhs_shape, rhs_shape, spec = case
     lhs = ab.normal(0.0, 1.0, lhs_shape)
     rhs = ab.normal(0.0, 1.0, rhs_shape)
     truth = ab.einsum(spec, lhs, rhs)
+    res = _einop(spec, lhs, rhs, 'sum', 'multiply')
+    assert truth.shape == res.shape
+    assert ab.allclose(truth, res, tol)
+
+
+@pytest.mark.parametrize('case', [
+    ((10,), (10,), 'i,j', 'sum', 'add'),
+    ((10,), (10,), 'i,j', 'sum', 'subtract'),
+    ((10,), (10,), 'i,j', 'sum', 'multiply'),
+    ((10,), (10,), 'i,j', 'sum', 'divide'),
+])
+def test_generic_outer(case):
+    tol = 20 * ab.finfo(ab.float32).eps
+
+    lhs_shape, rhs_shape, spec, reduction, pairwise = case
+    lhs = ab.normal(0.0, 1.0, lhs_shape)
+    rhs = ab.normal(0.0, 1.0, rhs_shape)
+    truth = getattr(ab, pairwise)(lhs[:, None], rhs[None, :])
+    res = _einop(spec, lhs, rhs, reduction, pairwise)
+    assert truth.shape == res.shape
+    assert ab.allclose(truth, res, tol)
+
+
+@pytest.mark.parametrize('case', [
+    ((10,), (10,), 'i,i', 'sum', 'add'),
+    ((10,), (10,), 'i,i', 'sum', 'subtract'),
+    ((10,), (10,), 'i,i', 'sum', 'multiply'),
+    ((10,), (10,), 'i,i', 'sum', 'divide'),
+    ((10,), (10,), 'i,i', 'min', 'add'),
+    ((10,), (10,), 'i,i', 'min', 'subtract'),
+    ((10,), (10,), 'i,i', 'min', 'multiply'),
+    ((10,), (10,), 'i,i', 'min', 'divide'),
+    ((10,), (10,), 'i,i', 'max', 'add'),
+    ((10,), (10,), 'i,i', 'max', 'subtract'),
+    ((10,), (10,), 'i,i', 'max', 'multiply'),
+    ((10,), (10,), 'i,i', 'max', 'divide'),
+])
+def test_generic_inner(case):
+    tol = 20 * ab.finfo(ab.float32).eps
+
+    lhs_shape, rhs_shape, spec, reduction, pairwise = case
+    lhs = ab.normal(0.0, 1.0, lhs_shape)
+    rhs = ab.normal(0.0, 1.0, rhs_shape)
+    truth = getattr(ab, reduction)(getattr(ab, pairwise)(lhs, rhs))
+    res = _einop(spec, lhs, rhs, reduction, pairwise)
+    assert truth.shape == res.shape
+    assert ab.allclose(truth, res, tol)
+
+
+@pytest.mark.parametrize('case', [
+    ((3,), (3,), 'i,i->i|i'),
+    ((1,), (5,), 'i,i->i|i'),
+    ((2, 2), (2, 2), 'ij,ij->ij|ij'),
+    ((2, 2), (2, 3), 'ij,ij->ij|ij'),
+    ((2, 2), (3, 3), 'ij,ij->ij|ij'),
+    ((2, 2), (3, 4), 'ij,ij->ij|ij'),
+    ((2, 2), (2, 2), 'ij,ij->ij|ij'),
+    ((2, 3), (2, 2), 'ij,ij->ij|ij'),
+    ((3, 3), (2, 2), 'ij,ij->ij|ij'),
+    ((3, 4), (2, 2), 'ij,ij->ij|ij'),
+    ((2, 3, 4), (4, 1, 5), 'ijk,ijk->ijk|ijk'),
+])
+def test_kron(case):
+    tol = 100 * ab.finfo(ab.float32).eps
+
+    lhs_shape, rhs_shape, spec = case
+    lhs = ab.normal(0.0, 1.0, lhs_shape)
+    rhs = ab.normal(0.0, 1.0, rhs_shape)
+    truth = ab.kron(lhs, rhs)
     res = _einop(spec, lhs, rhs, 'sum', 'multiply')
     assert truth.shape == res.shape
     assert ab.allclose(truth, res, tol)
