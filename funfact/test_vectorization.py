49cae9c772811f26e7ae1be542efe1e307fa7e7b
campsd <daancamps@gmail.com>
2022-01-10 15:56:40 -0800

    updated vectorization tests

--- funfact/test_vectorization.py
+++ funfact/test_vectorization.py
@@ -1,78 +1,111 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import pytest
+import pytest  # noqa: F401
 import numpy as np
 from funfact import tensor, indices, Factorization
 from .vectorization import vectorize, view
 
 
-def test_vectorize_random():
+@pytest.mark.parametrize('case', [
+    (7, -1, slice(0, -1), True),
+    (7, 0, slice(1, None), False)
+])
+def test_vectorize_random(case):
     a = tensor('a', 2, 3)
     b = tensor('b', 3, 4)
     i, j, k = indices('i, j, k')
     tsrex = a[i, j] * b[j, k]
-    nvec = 7
-    tsrex_vector = vectorize(tsrex, nvec)
+    nvec, vec_ax, data_ax, append = case
+    tsrex_vector = vectorize(tsrex, nvec, append)
     assert tsrex_vector.ndim == tsrex.ndim + 1
-    assert tsrex_vector.shape == (*tsrex.shape, nvec)
-    assert tsrex_vector.root.lhs.indexless.abstract.shape == (*a.shape, nvec)
-    assert tsrex_vector.root.rhs.indexless.abstract.shape == (*b.shape, nvec)
+    assert tsrex_vector.shape[data_ax] == tsrex.shape
+    assert tsrex_vector.shape[vec_ax] == nvec
+    assert tsrex_vector.root.lhs.indexless.abstract.shape[data_ax] == a.shape
+    assert tsrex_vector.root.lhs.indexless.abstract.shape[vec_ax] == nvec
+    assert tsrex_vector.root.rhs.indexless.abstract.shape[data_ax] == b.shape
+    assert tsrex_vector.root.rhs.indexless.abstract.shape[vec_ax] == nvec
 
     fac = Factorization.from_tsrex(tsrex_vector)
     assert fac.ndim == tsrex_vector.ndim
-    assert fac.all_factors[0].shape == (*a.shape, nvec)
-    assert fac.all_factors[1].shape == (*b.shape, nvec)
+    assert fac.all_factors[0].shape[data_ax] == a.shape
+    assert fac.all_factors[0].shape[vec_ax] == nvec
+    assert fac.all_factors[1].shape[data_ax] == b.shape
+    assert fac.all_factors[1].shape[vec_ax] == nvec
 
 
-def test_vectorize_concrete():
+@pytest.mark.parametrize('case', [
+    (7, -1, slice(0, -1), True),
+    (7, 0, slice(1, None), False)
+])
+def test_vectorize_concrete(case):
     a = tensor('a', 2, 3)
     b = tensor('b', np.eye(3))
     i, j, k = indices('i, j, k')
     tsrex = a[i, j] * b[j, k]
-    nvec = 7
-    tsrex_vector = vectorize(tsrex, nvec)
+    nvec, vec_ax, data_ax, append = case
+    tsrex_vector = vectorize(tsrex, nvec, append)
     assert tsrex_vector.ndim == tsrex.ndim + 1
-    assert tsrex_vector.shape == (*tsrex.shape, nvec)
-    assert tsrex_vector.root.lhs.indexless.abstract.shape == (*a.shape, nvec)
-    assert tsrex_vector.root.rhs.indexless.abstract.shape == (*b.shape, nvec)
+    assert tsrex_vector.shape[data_ax] == tsrex.shape
+    assert tsrex_vector.shape[vec_ax] == nvec
+    assert tsrex_vector.root.lhs.indexless.abstract.shape[data_ax] == a.shape
+    assert tsrex_vector.root.lhs.indexless.abstract.shape[vec_ax] == nvec
+    assert tsrex_vector.root.rhs.indexless.abstract.shape[data_ax] == b.shape
+    assert tsrex_vector.root.rhs.indexless.abstract.shape[vec_ax] == nvec
 
     fac = Factorization.from_tsrex(tsrex_vector)
     assert fac.ndim == tsrex_vector.ndim
-    assert fac.all_factors[0].shape == (*a.shape, nvec)
-    assert fac.all_factors[1].shape == (*b.shape, nvec)
+    assert fac.all_factors[0].shape[data_ax] == a.shape
+    assert fac.all_factors[0].shape[vec_ax] == nvec
+    assert fac.all_factors[1].shape[data_ax] == b.shape
+    assert fac.all_factors[1].shape[vec_ax] == nvec
 
 
-def test_view_random():
+@pytest.mark.parametrize('case', [
+    True,
+    False
+])
+def test_view_random(case):
     a = tensor('a', 2, 3)
     b = tensor('b', 3, 4)
     i, j, k = indices('i, j, k')
     tsrex = a[i, j] * b[j, k]
+    append = case
     nvec = 7
-    tsrex_vector = vectorize(tsrex, nvec)
+    tsrex_vector = vectorize(tsrex, nvec, append)
     vfac = Factorization.from_tsrex(tsrex_vector)
     for i in range(nvec):
-        fac = view(vfac, tsrex, i)
+        fac = view(vfac, tsrex, i, append)
         assert fac.ndim == tsrex.ndim
         assert fac.all_factors[0].shape == a.shape
         assert fac.all_factors[1].shape == b.shape
 
     # view last instance
-    view(vfac, tsrex, -1)
+    view(vfac, tsrex, -1, append)
 
+    # view out-of-bound
     with pytest.raises(IndexError):
-        view(vfac, tsrex, nvec)
+        view(vfac, tsrex, nvec, append)
+
+    # view with wrong append flag
+    with pytest.raises(IndexError):
+        view(vfac, tsrex, nvec-1, not append)
 
 
-def test_view_concrete():
+@pytest.mark.parametrize('case', [
+    True,
+    False
+])
+def test_view_concrete(case):
     a = tensor('a', 2, 3)
     b = tensor('b', np.eye(3))
     i, j, k = indices('i, j, k')
     tsrex = a[i, j] * b[j, k]
+    append = case
     nvec = 7
-    tsrex_vector = vectorize(tsrex, nvec)
+    tsrex_vector = vectorize(tsrex, nvec, append)
     vfac = Factorization.from_tsrex(tsrex_vector)
     for i in range(nvec):
-        fac = view(vfac, tsrex, i)
+        fac = view(vfac, tsrex, i, append)
         assert fac.ndim == tsrex.ndim
         assert fac.all_factors[0].shape == a.shape
         assert fac.all_factors[1].shape == b.shape
@@ -81,5 +114,10 @@ def test_view_concrete():
     # view last instance
     view(vfac, tsrex, -1)
 
+    # view out-of-bound
     with pytest.raises(IndexError):
         view(vfac, tsrex, nvec)
+
+    # view with wrong append flag
+    with pytest.raises(IndexError):
+        view(vfac, tsrex, nvec-1, not append)
