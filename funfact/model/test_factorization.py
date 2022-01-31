69c68157ccdd82583e1f14ddf6594a240d419087
campsd <daancamps@gmail.com>
2022-01-31 15:21:21 -0800

    factorization unit tests

--- funfact/model/test_factorization.py
+++ funfact/model/test_factorization.py
@@ -1,6 +1,114 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import pytest
+from unittest.mock import MagicMock as M
+from funfact import active_backend as ab
+from ._factorization import Factorization
+
+
+def _test_factorization_props(fac):
+    assert isinstance(fac, Factorization)
+    assert hasattr(fac, '_tsrex')
+    assert hasattr(fac, 'tsrex')
+    assert hasattr(fac, 'shape')
+    assert hasattr(fac, 'ndim')
+    assert hasattr(fac, 'ndim')
+    assert hasattr(fac, 'factors')
+    assert hasattr(fac, 'all_factors')
+    assert fac.forward() == fac()
+    assert isinstance(fac.factors, Factorization._NodeView)
+    assert isinstance(fac.all_factors, Factorization._NodeView)
+
+
+def test_init_factory():
+    fac = Factorization(M())
+    _test_factorization_props(fac)
+    fac = Factorization.from_tsrex(M(), dtype=M(), initialize=M())
+    _test_factorization_props(fac)
+
+
+def test_nodeview():
+    nodes = [M(data=0), M(data=1)]
+    nv = Factorization._NodeView('data', nodes)
+    assert isinstance(nv.__repr__(), str)
+    assert len(nv) == 2
+    for i, node in enumerate(nv):
+        assert i == node
+    for i in range(len(nv)):
+        assert nv[i] == i
+        nv[i] = i + 10
+        assert nv[i] == i + 10
+
+
+@pytest.mark.parametrize('test_case', [
+    (0, slice(0, 1, 2), slice(0, 1, 2)),
+    (0, 1, slice(1, 2, None)),
+    (0, -1, slice(-1, None, None)),
+    (0, Ellipsis, None),
+    (0, [0, 1], (0, 1)),
+    (0, 1.2, RuntimeError)
+])
+def test_as_slice(test_case):
+    i, axis,  result = test_case
+    as_slice = Factorization._as_slice
+    if isinstance(result, type) and issubclass(result, Exception):
+        with pytest.raises(result):
+            as_slice(i, axis)
+    else:
+        assert as_slice(i, axis) == result
+
+
+def _gen_factors_mock(data, optimizable=True):
+    root = M(data=data, decl=M(optimizable=optimizable))
+    root.name = 'tensor'
+    return Factorization(M(), _tsrex=M(root=root))
+
+
+def test_factors():
+    fac = _gen_factors_mock(1, True)
+    assert len(fac.factors) == 1
+    assert len(fac.all_factors) == 1
+    assert fac.factors[0] == fac.all_factors[0]
+    assert fac.factors[0] == 1
+    fac.factors = [2]
+    assert fac.factors[0] == 2
+    fac = _gen_factors_mock(1, False)
+    assert len(fac.factors) == 0
+    assert len(fac.all_factors) == 1
+    assert fac.all_factors[0] == 1
+
+
+def test_penalties():
+    def _prefer(data, *args):
+        return ab.sum(data)
+
+    root = M(data=ab.tensor([1, 2, 3]),
+             decl=M(optimizable=True, prefer=_prefer))
+    root.name = 'tensor'
+    fac = Factorization(M(), _tsrex=M(root=root))
+    assert fac.penalty() == 6
+
+
+def test_get_set_item():
+    root = M(data=1, decl=M(symbol='a', optimizable=True), ndim=1)
+    root.name = 'tensor'
+    fac = Factorization(M(), _tsrex=M(root=root))
+    assert fac['a'] == 1
+    fac['a'] = 2
+    assert fac['a'] == 2
+    with pytest.raises(AttributeError):
+        fac['unknown-factor']
+    with pytest.raises(AttributeError):
+        fac['unknown-factor'] = 2
+    with pytest.raises(IndexError):
+        fac[0, 0]
+    with pytest.raises(IndexError):
+        fac[0, ...]
+    #assert fac[...] == 1
+
+
+# TODO: integration tests on top level
+''' NEW TESTS W/O MAGICMOCK
 import funfact as ff
 from funfact import active_backend as ab
 
@@ -49,13 +157,7 @@ def test_factors_element_access():
     assert ab.allclose(ab.squeeze(fac[:, 0]), fac()[:, 0])
     assert ab.allclose(ab.squeeze(fac[1, ...]), fac()[1, ...])
     with pytest.raises(IndexError):
-        fac[0, 'A']
-    with pytest.raises(IndexError):
         fac[0, 0, 0]
-    with pytest.raises(IndexError):
-        fac[5:6, 0]
-    with pytest.raises(IndexError):
-        fac[0:3, 0]
 
 
 def test_multiple_factors():
@@ -79,8 +181,9 @@ def test_penalties():
     assert fac.penalty() == fac.penalty(sum_leafs=False)[0] + \
            fac.penalty(sum_leafs=False)[1]
 
-
 '''
+
+''' OLD TESTS
 def test_elementwise():
     tol = 20 * np.finfo(np.float32).eps
 
