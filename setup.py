c707b7fe404f382fe3ba3128bf2104c22cd078b8
Yu-Hang "Maxin" Tang <Tang.Maxin@gmail.com>
2022-02-03 11:20:58 -0800

    Update `mkdocs-jupyter` to 0.20.0 to support light/dark mode switch for notebooks.

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
             'mkdocs-material==8.0.5',
             'mkdocs-literate-nav==0.4.1',
             'mkdocs-section-index==0.3.2',
-            'mkdocs-jupyter==0.19.0',
+            'mkdocs-jupyter==0.20.0',
             'mkdocstrings==0.17.0',
         ],
         'devel': [
