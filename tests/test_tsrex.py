b9585fcc8369eb712e3efab91f6fc714f7fa7eeb
campsd <daancamps@gmail.com>
2022-02-07 09:08:45 -0800

    clean up tests

--- tests/test_tsrex.py
+++ tests/test_tsrex.py
@@ -8,22 +8,18 @@ from funfact import active_backend as ab
 
 @pytest.fixture
 def my_arrays():
-    a = np.reshape(np.arange(0, 6), (2, 3))
-    b = np.reshape(np.arange(6, 14), (2, 4))
-    return a, b
+    a = ab.tensor(np.reshape(np.arange(0, 6), (2, 3)), dtype=ab.float32)
+    b = ab.tensor(np.reshape(np.arange(6, 14), (2, 4)), dtype=ab.float32)
+    c = ab.tensor(np.reshape(np.arange(15, 75), (3, 4, 5)), dtype=ab.float32)
+    return a, b, c
 
 
 @pytest.fixture
-def my_tensors(my_arrays):
-    a_arr, b_arr = my_arrays
+def my_tsrexs(my_arrays):
+    a_arr, b_arr, c_arr = my_arrays
     a = ff.tensor('a', 2, 3, initializer=a_arr)
     b = ff.tensor('b', 2, 4, initializer=b_arr)
-    return a, b
-
-
-@pytest.fixture
-def my_tsrexs(my_tensors):
-    a, b = my_tensors
+    c = ff.tensor('c', 3, 4, 5, initializer=c_arr)
     i, j, k, p, q = ff.indices('i, j, k, p, q')
     return [
         a[k, i] * b[k, j],
@@ -31,24 +27,26 @@ def my_tsrexs(my_tensors):
         a[~k, ~i] * a[k, i],
         a[k, i] * a[k, i],
         a[k, i] * b[k, j] >> [j, i],
-        a[k, i] * b[k, j] >> [p, q]
+        a[k, i] * b[k, j] >> [p, q],
+        a[k, i] * c[i, j, p]
     ]
 
 
 @pytest.fixture
 def my_expected(my_arrays):
-    a, b = my_arrays
+    a, b, c = my_arrays
     return [
-        ((3, 4), ab.tensor(np.transpose(a) @ b, dtype=ab.float32)),
-        ((2, 3), ab.tensor(a * a, dtype=ab.float32)),
-        ((2, 3), ab.tensor(a * a, dtype=ab.float32)),
-        ((),     ab.tensor(np.sum(a * a), dtype=ab.float32)),
-        ((4, 3), ab.tensor(np.transpose(b) @ a, dtype=ab.float32)),
-        SyntaxError
+        ((3, 4), ab.transpose(a, (1, 0)) @ b),
+        ((2, 3), a * a),
+        ((2, 3), a * a),
+        ((),     ab.sum(a * a)),
+        ((4, 3), ab.transpose(b, (1, 0)) @ a),
+        SyntaxError,
+        ((2, 4, 5), ab.einsum('ki, ijp->kjp', a, c))
     ]
 
 
-def test_fixture(my_tsrexs, my_expected):
+def test_tsrex_shape_result(my_tsrexs, my_expected):
     for tsrex, expected in zip(my_tsrexs, my_expected):
         if isinstance(expected, type) and issubclass(expected, Exception):
             with pytest.raises(expected):
